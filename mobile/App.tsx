51afe01bbf015064f17e93c6f7484d1230bd2819
Caleb Lima <lima.cardoso.caleb@gmail.com>
2022-05-06 00:21:48 -0300

    Cria e estiliza ThouchableOpacity e BottomSheet

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,9 +1,10 @@
+import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import { View } from 'react-native';
 import AppLoading from 'expo-app-loading';
 import { useFonts, Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter';
 import { theme } from './src/theme/index';
-import { Widget } from './src/components/Widget/Widget';
+import Widget from './src/components/Widget/Widget';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -21,13 +22,13 @@ export default function App() {
       backgroundColor: theme.colors.background,
     }}>
 
-      <Widget />
-
       <StatusBar
         style="light"
         backgroundColor="transparent"
         translucent
       />
+
+      <Widget />
     </View>
   );
 }
