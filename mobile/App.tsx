8096dd2678a140b4a1ace3aab9cd21104bb70d2d
Caleb Lima <lima.cardoso.caleb@gmail.com>
2022-05-05 23:33:51 -0300

    Configurando fontes personalizadas

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,8 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import { View } from 'react-native';
+import AppLoading from 'expo-app-loading';
+import { useFonts, Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter';
 import { theme } from './src/theme/index';
 import { Widget } from './src/components/Widget/Widget';
+
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
   return (
     <View style={{
       flex: 1,
